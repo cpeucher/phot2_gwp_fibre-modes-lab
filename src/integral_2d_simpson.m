040cc1400fff5b87b31182cb4737749a64df2d54
cpeucher <cpeucher@univ-rennes.fr>
2025-01-13 21:56:50 +0100

    Fixed integral_2d_simpson

--- src/integral_2d_simpson.m
+++ src/integral_2d_simpson.m
@@ -1,4 +1,4 @@
-function I = integral_2d_simpson(F)
+function I = integral_2d_simpson(X,Y,F)
 % Double integral of a function defined on a Cartesian coordinate system by Simpson method
 %
 % -------------------------------------------------------------------------
@@ -10,11 +10,26 @@ function I = integral_2d_simpson(F)
 % -------------------------------------------------------------------------
 % FUNCTION CALL:
 % -------------------------------------------------------------------------
-% 
+% xrange = [-13, 14];  
+% yrange = [-15, 10]; 
+% nxpoints = 2001;
+% nypoints = 1001;
+% x = linspace(xrange(1),xrange(2),nxpoints);
+% y = linspace(yrange(1),yrange(2),nypoints);
+% [X,Y] = meshgrid(x,y);
+% [THETA,RHO] = cart2pol(X,Y);
+% F = exp(-RHO.^2);
+% I = integral_2d_simpson(X,Y,F); % should be pi
 %
 % -------------------------------------------------------------------------
 % INPUTS:
 % -------------------------------------------------------------------------
+% X                 abscissa of the points on the 2D Cartesian space grid
+%                       (obtained through meshgrid) [real matrix]
+% 
+% Y                 ordinates of the points on the 2D Cartesian space grid
+%                       (obtained through meshgrid) [real matrix]
+%
 % F                 values of the function to integrate, defined as a 
 %                       matrix on the Cartesian grid space_grid 
 %                       [real matrix]
@@ -27,7 +42,7 @@ function I = integral_2d_simpson(F)
 % -------------------------------------------------------------------------
 % GLOBAL:
 % -------------------------------------------------------------------------
-% space_grid       2D space grid in the transverse plane [structure]
+% 
 %
 % -------------------------------------------------------------------------
 % CREDITS:
@@ -42,30 +57,28 @@ function I = integral_2d_simpson(F)
 % -------------------------------------------------------------------------
 % -------------------------------------------------------------------------
 
-global space_grid
-
-[m,n] = size(space_grid.X);
-% Extract space grid size.
+[m,n] = size(X);
+% Size of the space grid
 
 if ~rem(m,2)
     error('num_int2d_Simpson: the number of points on the grid should be odd.')
 end
 
-m = m - 1;        % m is the number of intervals along the x axis.
-n = n - 1;        % n is the number of intervals along the y axis.
+xmin = X(1,1);
+xmax = X(1,n);
+% Extract min and max values of the grid along the x axis
 
-xmin = space_grid.x(1);
-xmax = space_grid.x(end);
-% Extract min and max values of the grid along the x axis.
+ymin = Y(1,1);
+ymax = Y(m,1);
+% Extract min and max values of the grid along the y axis
 
-ymin = space_grid.y(1);
-ymax = space_grid.y(end);
-% Extract min and max values of the grid along the y axis.
+m = m - 1;        % m is now the number of intervals along the x axis.
+n = n - 1;        % n is now the number of intervals along the y axis.
 
 W = mydblsimpweights(m,n);
 % Weights of the Riemann sum.
 
-I=(ymax-ymin)*(xmax-xmin)*sum(sum(W.*F))/(9*m*n);
+I=(ymax - ymin)*(xmax - xmin)*sum(sum(W.*F))/(9*m*n);
 % Integral according to Simpson rule.
 
 end
